b7a16f78131e9ececa93ecf5f53b69aad72db3bc
Alessandro Moran <elaeht2002@gmail.com>
2026-01-25 15:13:39 -0500

    Agregamos modelo Notificacion en models , esto permitira recibir notificaciones en nuestro Sistema

---
+++ src/main/java/app/models/Notification.java
@@ -0,0 +1,72 @@
+package app.models;
+
+import java.time.LocalDateTime;
+
+public class Notification {
+    private int idNotification;
+    private String userUuid;
+    private String type;
+    private String title;
+    private String message;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+
+    public Notification() {}
+
+    public int getIdNotification() {
+        return idNotification;
+    }
+
+    public void setIdNotification(int idNotification) {
+        this.idNotification = idNotification;
+    }
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public void setUserUuid(String userUuid) {
+        this.userUuid = userUuid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+}
