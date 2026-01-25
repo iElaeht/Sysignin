a42f5a7be715ff4ec8d577786f3a83524b0c1598
Alessandro Moran <elaeht2002@gmail.com>
2026-01-25 15:15:50 -0500

    Agregamos modelo AuditLog en models

---
+++ src/main/java/app/models/AuditLog.java
@@ -0,0 +1,81 @@
+package app.models;
+
+import java.time.LocalDateTime;
+
+public class AuditLog {
+    private int idLog;
+    private int idUser;
+    private String userIdentifier;
+    private String action;
+    private String ipSource;
+    private String userAgent;
+    private String details;
+    private LocalDateTime createdAt;
+
+    public AuditLog() {}
+
+    public int getIdLog() {
+        return idLog;
+    }
+
+    public void setIdLog(int idLog) {
+        this.idLog = idLog;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUserIdentifier() {
+        return userIdentifier;
+    }
+
+    public void setUserIdentifier(String userIdentifier) {
+        this.userIdentifier = userIdentifier;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getIpSource() {
+        return ipSource;
+    }
+
+    public void setIpSource(String ipSource) {
+        this.ipSource = ipSource;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+}
